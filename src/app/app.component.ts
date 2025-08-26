22d3f5fa0b6727f417c7a3dfdd2bb34210becf4f
harto.dev <matthias.hartwig@bluewin.ch>
2025-08-26 21:33:15 +0200

    add version information display and load from manifest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouterLink, RouterLinkActive} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
 import {
   IonApp,
   IonContent,
@@ -31,7 +32,7 @@ import {
   styleUrls: ['app.component.scss'],
   imports: [RouterLink, RouterLinkActive, IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterLink, IonRouterOutlet, IonNote],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public appPages = [
     {title: 'Home', url: '/home', icon: 'home'},
     {title: 'About', url: '/about', icon: 'information-circle'},
@@ -39,7 +40,13 @@ export class AppComponent {
 
   ];
 
-  constructor() {
+  public versionInfo = {
+    version: '',
+    releaseDate: '',
+    versionName: ''
+  };
+
+  constructor(private http: HttpClient) {
     addIcons({
       homeSharp,
       homeOutline,
@@ -48,6 +55,25 @@ export class AppComponent {
       informationCircle,
       settingsSharp,
       settingsOutline
+    });
+  }
+
+  ngOnInit() {
+    this.loadVersionInfo();
+  }
+
+  private loadVersionInfo() {
+    this.http.get<any>('/manifest.webmanifest').subscribe({
+      next: (manifest) => {
+        this.versionInfo = {
+          version: manifest.version || 'N/A',
+          releaseDate: manifest.releasedate ? new Date(manifest.releasedate).toLocaleDateString() : 'N/A',
+          versionName: manifest.version_name || 'N/A'
+        };
+      },
+      error: (error) => {
+        console.error('Error loading manifest:', error);
+      }
     });
   }
 }
